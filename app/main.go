1d712889ba0536094f85a2870cab535db2760f3b
PhizickZ <12936301+phizickz@users.noreply.github.com>
2023-10-12 19:42:42 +0200

    fix image loading

--- app/main.go
+++ app/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
-	"html/template"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -15,62 +17,108 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-
 func getDayAsString() string {
 	t := time.Now().Local().Weekday().String()
 	return t
 }
 
-func apiHandler (w http.ResponseWriter, r *http.Request) {
+func getMonthAsNumber() int {
+	return int(time.Now().Month())
+}
+
+func textHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		http.Error(w, "Method is not supported.", http.StatusNotFound)
 		return
 	}
-	subPath := r.URL.Path[len("/api"):]
 
- 	if subPath == "/title" {
-		tmpl,_ := template.New("").Parse("<title>{{.}}</title>")
-		tmpl.Execute(w,"HDED")
+	switch r.URL.Path[len("/text"):] {
+	case "/title":
+		tmpl, _ := template.New("").Parse("<title>{{.}}</title>")
+		tmpl.Execute(w, "HDED")
+
+	case "/top":
+		tmpl, _ := template.New("").Parse("<h1>{{.}}</h1>")
+		tmpl.Execute(w, "Which day is it?")
+
+	case "/center":
+		tmpl, _ := template.New("").Parse("<h1>{{.}}</h1>")
+		tmpl.Execute(w, getDayAsString())
 	}
-
-	if subPath == "/toptext" {
-		tmpl,_ := template.New("").Parse("<h1>{{.}}</h1>")
-		tmpl.Execute(w,"Which day is it?")
-	}
-
-	if subPath == "/centertext" {
-		tmpl,_ := template.New("").Parse("<h1>{{.}}</h1>")
-		tmpl.Execute(w,getDayAsString())
-	}
-
 
 }
 
-func imageHandler (w http.ResponseWriter, r *http.Request) {	
+func getBackgroundImage() string {
+	switch getMonthAsNumber() {
+	case 5:
+		return "resources/images/seasonal/norwegian-constitution/Norges-flagg.jpg"
+	case 1, 12:
+		return "resources/images/seasonal/winter/635848557150633136-120303261_winter.jpg"
+	}
+	return ""
+}
+
+func imageHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		http.Error(w, "Method is not supported.", http.StatusNotFound)
 		return
 	}
-	subPath := r.URL.Path[len("/image"):]
-	imagePath := "resources/images/sprites/pepe.png"
+	imageToServe := ""
 
-	if subPath == "/icon" {
-		imagePath = "resources/images/sprites/thinkong.png"
+	switch r.URL.Path[len("/image"):] {
+	case "/icon":
+		imageToServe = "resources/images/sprites/thinkong.png"
+	
+	case "/center":
+		if strings.ToLower(getDayAsString()) == "wednesday" {
+			imageToServe = "resources/images/easter-egg/wedmydudes.jpg"
+		} else {
+			return
+		}
+
+	case "/background":
+		imageToServe = getBackgroundImage()
+		if imageToServe == "" {
+			return
+		}
 	}
 
-	if subPath == "/background" {
-		imagePath = "resources/images/easter-egg/wedmydudes.jpg"
+	imageFileExists, err := fileExists(imageToServe)
+
+	if err != nil {
+		fmt.Printf("Error checking if the imagefile exists: %v\n", err)
+		return
 	}
 
-	http.ServeFile(w, r, imagePath)
+	if imageFileExists {
+		http.ServeFile(w, r, imageToServe)
+	} else {
+		fmt.Printf("Imagefile does not exist: %v\n", err)
+		return
+	}
+
+}
+
+func fileExists(filePath string) (bool, error) {
+	_, err := os.Stat(filePath)
+	if err == nil {
+		// File exists
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		// File does not exist
+		return false, nil
+	}
+	// Error occurred while checking
+	return false, err
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir("static")))
 
-	mux.Handle("/api", http.NotFoundHandler())
-	mux.HandleFunc("/api/", apiHandler)
+	mux.Handle("/text", http.NotFoundHandler())
+	mux.HandleFunc("/text/", textHandler)
 
 	mux.Handle("/image", http.NotFoundHandler())
 	mux.HandleFunc("/image/", imageHandler)
